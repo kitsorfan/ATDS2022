a8528258bef50f1dcc2a885ca22b3ef85dce94d0
kitsorfan <kitsorfan@protonmail.com>
2022-03-17 01:00:24 +0200

    update data to files

--- code/csv_to_parquet.py
+++ code/csv_to_parquet.py
@@ -7,12 +7,12 @@ from pyspark.sql import SparkSession
 
 spark=SparkSession.builder.appName("csv2parquet").getOrCreate()
 
-movies_genres=spark.read.csv('hdfs://master:9000/data/movie_genres.csv')
-movies_genres.write.parquet('hdfs://master:9000/data/movie_genres.parquet')
+movies_genres=spark.read.csv('hdfs://master:9000/files/movie_genres.csv')
+movies_genres.write.parquet('hdfs://master:9000/files/movie_genres.parquet')
 
-movies=spark.read.csv('hdfs://master:9000/data/movies.csv')
-movies.write.parquet('hdfs://master:9000/data/movies.parquet')
+movies=spark.read.csv('hdfs://master:9000/files/movies.csv')
+movies.write.parquet('hdfs://master:9000/files/movies.parquet')
 
-ratings=spark.read.csv('hdfs://master:9000/data/ratings.csv')
-ratings.write.parquet('hdfs://master:9000/data/ratings.parquet')
+ratings=spark.read.csv('hdfs://master:9000/files/ratings.csv')
+ratings.write.parquet('hdfs://master:9000/files/ratings.parquet')
 
