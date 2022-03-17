a6aad0c3269e55d6e5af13c5a6c8c832557c81e4
kitsorfan <kitsorfan@protonmail.com>
2022-03-17 20:29:28 +0200

    finish Q3 SQL

--- code/Part1/Q3/Q3_SQL_csv.py
+++ code/Part1/Q3/Q3_SQL_csv.py
@@ -1,42 +1,64 @@
+# NTUA ECE, 9th Semester, ATDS
+# Stylianos Kandylakis, Kitsos Orfanopoulos, Christos Tsoufis
+
 from pyspark.sql import SparkSession
+
 import time
-import sys
 
-spark = SparkSession.builder.appName("query1-sql").getOrCreate()
+"""
+movie_genres.csv
+0-movieID, 1-genre
 
-start_time = time.time()
+movies.csv
+0-movieID, 1-title, 2-description, 3-releaseDate, 4-duration, 5-cost, 6-revenue
 
-if sys.argv[1] == "csv":
-        movie_genres = spark.read.format("csv").options(header='false', inferSchema='true').load("hdfs://master:9000/project/movie_genres.csv")
-        ratings = spark.read.format("csv").options(header='false', inferSchema='true').load("hdfs://master:9000/project/ratings.csv")
-elif sys.argv[1] == "parquet":
-        movie_genres = spark.read.parquet("hdfs://master:9000/project/movie_genres.parquet")
-        ratings = spark.read.parquet("hdfs://master:9000/project/ratings.parquet")
-else:
-        print("Invalid argument")
+ratings.csv
+0-userID, 1-movieID, 2-rating, 3-ratingDate
+"""
+
+spark = SparkSession.builder.appName("Q3_SQL_csv").getOrCreate()
+
+startTime = time.time()
+
+movie_genres = spark.read.format("csv").options(header='false', inferSchema='true').load("hdfs://master:9000/files/movie_genres.csv")
+ratings = spark.read.format("csv").options(header='false', inferSchema='true').load("hdfs://master:9000/files/ratings.csv")
 
 movie_genres.createOrReplaceTempView("movie_genres")
 ratings.createOrReplaceTempView("ratings")
 
-rating_per_movie = "select avg(R._c2) as avg_rating, R._c1 as movie_id from ratings as R group by R._c1"
-rating_per_movie = spark.sql(rating_per_movie)
-rating_per_movie.createOrReplaceTempView("rating_per_movie")
+query1 = """
+        SELECT 
+                AVG(r._c2) AS meanRating, 
+                r._c1 AS movieID 
+        FROM ratings AS r
+        GROUP BY r._c1
+"""
+ratingsPerMovie = spark.sql(query1)
+ratingsPerMovie.createOrReplaceTempView("ratingsPerMovie")
 
-rating_per_genre = "select rpm.avg_rating as RATING, movie_genres._c1 as GENRE from rating_per_movie as rpm join movie_genres on rpm.movie_id = movie_genres._c0"
-rating_per_genre = spark.sql(rating_per_genre)
-rating_per_genre.createOrReplaceTempView("rating_per_genre")
+query2 = """
+        SELECT 
+                rpm.meanRating AS meanRating, 
+                movie_genres._c1 AS genre 
+        FROM ratingsPerMovie AS rpm 
+        JOIN movie_genres ON rpm.movieID = movie_genres._c0
+"""
+ratingPerGenre = spark.sql(query2)
+ratingPerGenre.createOrReplaceTempView("ratingPerGenre")
 
-temp = "select RG.GENRE as Movie_Genre, avg(RG.RATING) as Average_Genre_Rating, count(*) as Movies_Count from rating_per_genre as RG group by RG.GENRE"
-temp = spark.sql(temp)
-temp.createOrReplaceTempView("temp")
+query3 = """
+        SELECT 
+                rpg.genre AS Genre, 
+                AVG(rpg.meanRating) AS MeanRatingPerGenre, 
+                COUNT(*) AS NumberOfMoviesPerGenre 
+        FROM ratingPerGenre AS rpg 
+        GROUP BY rpg.genre
+        ORDER BY Genre
+"""
 
-#RG.RATING is not null and RG.GENRE is not null
-
-sqlString = "select * from temp as t order by t.Movie_Genre"
-
-res = spark.sql(sqlString)
+res = spark.sql(query3)
 res.show()
 
-print("#########################################")
-print("Elapsed time = %.2f" % (time.time() - start_time))
-print("#########################################")
+endTime = time.time() 
+
+print("Total time: " + str(round(endTime-startTime,3))+" sec")
