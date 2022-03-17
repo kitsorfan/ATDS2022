fe9c2085397f833a8660f73cf17e32773760e23e
kitsorfan <kitsorfan@protonmail.com>
2022-03-17 18:56:54 +0200

    add Q3_RDD

--- code/Part1/Q3/Q3_RDD.py
+++ code/Part1/Q3/Q3_RDD.py
@@ -1,40 +1,62 @@
+# NTUA ECE, 9th Semester, ATDS
+# Stylianos Kandylakis, Kitsos Orfanopoulos, Christos Tsoufis
+
 from pyspark.sql import SparkSession
+
 from io import StringIO
 import csv
 import time
 
-spark = SparkSession.builder.appName("query1-rdd").getOrCreate()
+"""
+movie_genres.csv
+0-movieID, 1-genre
 
+movies.csv
+0-movieID, 1-title, 2-description, 3-releaseDate, 4-duration, 5-cost, 6-revenue
+
+ratings.csv
+0-userID, 1-movieID, 2-rating, 3-ratingDate
+"""
+
+spark = SparkSession.builder.appName("Q3_RDD").getOrCreate()
 sc = spark.sparkContext
 
-start_time = time.time()
+startTime = time.time()
 
-def split_complex(x):
+def splitComma(x):
         return list(csv.reader(StringIO(x), delimiter=','))[0]
 
-
-mean_rating_per_movie = \
-        sc.textFile("hdfs://master:9000/project/ratings.csv"). \
-        map(lambda x: split_complex(x)). \
+# A1. Split ratings
+# A2. create (movieID, (rating, 1))
+# A3. aggregate (totalRatingValue, totalNumberOfRatings)
+# A4. compute meanRatingPerMovie = totalRatingValue/totalNumberOfRatings
+meanRatingPerMovie = \
+        sc.textFile("hdfs://master:9000/files/ratings.csv"). \
+        map(lambda x: splitComma(x)). \
         map(lambda x: (x[1],(x[2],1))). \
         reduceByKey(lambda x,y:  (float(x[0])+float(y[0]),int(x[1])+int(y[1]))). \
         map(lambda x: (x[0], float(x[1][0])/int(x[1][1])))
 
-movie_genre = \
-        sc.textFile("hdfs://master:9000/project/movie_genres.csv"). \
-        map(lambda x: split_complex(x)). \
+# B1. Split movies genres
+# B2. create (movieID, genre)
+movieGenres = \
+        sc.textFile("hdfs://master:9000/files/movie_genres.csv"). \
+        map(lambda x: splitComma(x)). \
         map(lambda x: (x[0],x[1]))
 
-res = mean_rating_per_movie.join(movie_genre). \
+# C1. join meanRatingPerMovie with movieGenres
+# C2. create (genre, (meanRatingPerMovie, 1))
+# C3. aggregate (totalRatingPerGenre, totalMoviesPerGenre)
+# C4. create (genre, meanRatingPerGenre = round(totalRatingPerGenre/totalMoviesPerGenre), totalMoviesPerGenre)
+rdd = meanRatingPerMovie.join(movieGenres). \
         map(lambda x: (x[1][1],(x[1][0],1))). \
         reduceByKey(lambda x,y: (float(x[0])+float(y[0]),int(x[1])+int(y[1]))). \
-        map(lambda x: (x[0],(float(x[1][0])/int(x[1][1]),x[1][1])))
+        map(lambda x: (x[0],(round(float(x[1][0])/int(x[1][1]),3),x[1][1])))
 
 
-for i in res.collect():
+for i in rdd.collect():
         print(i)
 
-print("##############################")
-print("Elapsed time = %.2f" % (time.time()-start_time))
-print("##############################")
-~
+endTime = time.time() 
+
+print("Total time: " + str(round(endTime-startTime,3))+" sec")
