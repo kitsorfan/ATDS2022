95495009511877b008afa01822d63886a59cd789
kitsorfan <kitsorfan@protonmail.com>
2022-03-17 03:50:34 +0200

    finish csv_to_parquet

--- code/csv_to_parquet.py
+++ code/csv_to_parquet.py
@@ -5,14 +5,19 @@
 
 from pyspark.sql import SparkSession
 
-spark=SparkSession.builder.appName("csv2parquet").getOrCreate()
+spark=SparkSession.builder.appName("csv_to_parquet").getOrCreate()
 
+# MOVIES_GENRES
 movies_genres=spark.read.csv('hdfs://master:9000/files/movie_genres.csv')
 movies_genres.write.parquet('hdfs://master:9000/files/movie_genres.parquet')
 
+# MOVIES
 movies=spark.read.csv('hdfs://master:9000/files/movies.csv')
 movies.write.parquet('hdfs://master:9000/files/movies.parquet')
 
+# RATINGS
 ratings=spark.read.csv('hdfs://master:9000/files/ratings.csv')
 ratings.write.parquet('hdfs://master:9000/files/ratings.parquet')
 
+
+# COmpile & Run from master:  spark-submit csv_to_parquet.py
